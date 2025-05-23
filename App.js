f71fed4b4165257533ef07b132c68b14b1ea45ac
unknown <snehasishchakraborty1@gmail.com>
2025-05-24 01:56:07 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,23 @@
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const heading = React.createElement("h1",{id: "header"},"Hello World from React!");
+// root.render(heading);
+
+
+const parent = 
+    React.createElement("div",{id: "parent"},
+        [React.createElement("div",{id: "child"},
+            [React.createElement("h1",{id: "header"},"h1 tag"),
+             React.createElement("h2",{id: "header"},"h2 tag")
+            ]
+        ),
+        React.createElement("div",{id: "child"},
+            [React.createElement("h1",{id: "header"},"h1 tag"),
+             React.createElement("h2",{id: "header"},"h2 tag")
+            ]
+        )
+        ]
+    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
